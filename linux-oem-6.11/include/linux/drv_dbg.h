cfdd7da3826a8800c8663fa153e6837199d37fc0
bharathi-l <bhartz.bl@gmail.com>
2024-12-16 19:51:16 +0530

    Added .h file

--- linux-oem-6.11/include/linux/drv_dbg.h
+++ linux-oem-6.11/include/linux/drv_dbg.h
@@ -1,6 +1,14 @@
 #ifndef DRV_DBG_H
 #define DRV_DBG_H
 
+#define HANDLERS_DBG_PRINT 1
+
+extern spinlock_t my_lock;
+
+struct iwl_rx_handlers;
+
+extern void debug_handler_print(const struct iwl_rx_handlers* handlers, size_t num_handlers);
+
 static inline const char* get_thread_name(void);
 
 static inline const char* get_thread_name(void)
@@ -10,5 +18,6 @@ static inline const char* get_thread_name(void)
 	}
 	return "unknown"; 
 }
+
 
 #endif /* DRV_DBG_H */
